3c3d0580e3995e741b805a486f56f0876d2f6a26
Simon Montinari <si.montinari@gmail.com>
2025-10-07 12:02:11 +0100

    solved exercises

--- exercises/6a.sql
+++ exercises/6a.sql
@@ -2,3 +2,4 @@
 -- Include a count of how many albums they have released
 -- Order your results by artist name in ascending order
 -- (You should group the artists by id, not by name)
+SELECT artists.name, COUNT (album.id)
