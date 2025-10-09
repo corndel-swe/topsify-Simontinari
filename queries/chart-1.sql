40c50c5b6fab816a32fed8bb405d3df042f2ed6c
Simon Montinari <si.montinari@gmail.com>
2025-10-09 10:42:01 +0100

    solved 7a

--- queries/chart-1.sql
+++ queries/chart-1.sql
@@ -1,7 +1,7 @@
 .mode json
 
-SELECT artists.name AS artist_name, COUNT (tracks.id) AS explicit_tracks_albums
-FROM artists
+SELECT artists.name AS artist_name, COUNT (tracks.explicit) AS explicit_tracks_count
+FROM albums
 INNER JOIN tracks, albums
 WHERE artists.explicit = 1
 GROUP BY artists.id
