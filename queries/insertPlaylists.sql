54129b06564c17f6f348d064885e52f26eb69c83
Zzebb <blackmorea2@gmail.com>
2025-10-09 15:58:43 +0100

    Add SQL data inserts

---
+++ queries/insertPlaylists.sql
@@ -0,0 +1,3 @@
+INSERT INTO playlists(user_id, name)
+VALUES("4", "ChillMusic"),
+("5", "SadMusic")
---
+++ queries/insert_playlistTracks.sql
@@ -0,0 +1,3 @@
+INSERT INTO playlist_tracks(playlist_id, track_id)
+VALUES("1", "2mqTtvbKxH7SoEQ2oGAnsA"),
+("2", "3k4CoiIQXiNBrwrmbBMDxj")
